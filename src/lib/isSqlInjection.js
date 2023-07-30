d7af6147f1bfc67fe0a511653c8986f27b723c80
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2023-07-30 14:36:55 +0100

    Create isSqlInjection.js

---
+++ src/lib/isSqlInjection.js
@@ -0,0 +1,6 @@
+// Validate if the string is a SQL Injection
+
+function isSqlInjection(str) {
+  const sqlRegex = /(\b(ALTER|CREATE|DELETE|DROP|EXEC(UTE){0,1}|INSERT( +INTO){0,1}|MERGE|SELECT|UPDATE)\b)|(\b(OR|AND)( +\d+=[^ ]+){0,1}\b)|(--.*)|(;)/i;
+  return sqlRegex.test(str);
+}
